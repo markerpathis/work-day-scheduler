b979cc21aa23e6bafd432ac1dece69ae57d0b510
Parker Mathis <ParkerMathis@Joannas-MacBook-Pro.local>
2022-10-28 19:45:50 -0700

    Added function to get value from textbox on click

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,2 +1,18 @@
+var inputGroupEl = $(".task-form");
+var taskInputEl = $('textarea[name="task-input"]');
+
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
+
+function handleTaskSubmit(event) {
+  event.preventDefault();
+  var taskItem = taskInputEl.val();
+  if (!taskItem) {
+    console.log("task not entered");
+    return;
+  } else {
+    console.log(taskItem);
+  }
+}
+
+inputGroupEl.on("submit", handleTaskSubmit);
