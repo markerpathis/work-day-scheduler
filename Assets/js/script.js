7aad0510f8b578352490b9589a35749d72c5e8fc
Parker Mathis <ParkerMathis@Joannas-MacBook-Pro.local>
2022-10-28 22:25:54 -0700

    Updated event listener to work on all buttons

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-//HOOS TO FORMS FOR EVENT LISTENERS
+//HOOKS TO FORMS FOR EVENT LISTENERS
 var form9AM = $(".task-form-9am");
 var form10AM = $(".task-form-10am");
 
@@ -6,17 +6,15 @@ var form10AM = $(".task-form-10am");
 var textArea9am = $('textarea[name="task-input-9am"]');
 var textArea10am = $('textarea[name="task-input-10am"]');
 
+// var buttonsEl = document.querySelector("button");
+var buttonsEl = document.getElementsByClassName("btn");
+
+console.log(buttonsEl.length);
+
 var taskList = [];
 
 var today = moment();
 $("#currentDay").text(today.format("dddd, MMMM Do"));
-
-function handleTaskSubmit(event) {
-  event.preventDefault();
-  taskList[0] = textArea9am.val();
-  taskList[1] = textArea10am.val();
-  storeTaskList();
-}
 
 function storeTaskList() {
   localStorage.setItem("taskList", JSON.stringify(taskList));
@@ -33,11 +31,24 @@ function init() {
   retreiveTaskList();
 }
 
-///////////////////////////////////////////////////////////////////////////////////////
-// Event Listeners
-///////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+//Event Listeners
+/////////////////////////////////////////////////////////////////////////////////////
 
-form9AM.on("submit", handleTaskSubmit);
-form10AM.on("submit", handleTaskSubmit);
+for (var i = 0; i < buttonsEl.length; i++) {
+  buttonsEl[i].addEventListener("click", function (event) {
+    event.preventDefault();
+    var buttonClicked = event.target;
+    console.log(buttonClicked.value);
+    if (buttonClicked.value == 0) {
+      taskList[0] = textArea9am.val();
+      console.log(taskList);
+      storeTaskList();
+    } else if (buttonClicked.value == 1) {
+      taskList[1] = textArea10am.val();
+      storeTaskList();
+    }
+  });
+}
 
 init();
