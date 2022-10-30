6e01547512dac8cd7d2f4a1f8cadab8144aad11e
Parker Mathis <ParkerMathis@Joannas-MacBook-Pro.local>
2022-10-29 19:21:00 -0700

    Added timeblocks and logic through 12pm

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -9,6 +9,8 @@ var form10AM = $(".task-form-10am");
 // HOOKS TO TEXT AREAS
 var textArea9am = $('textarea[name="task-input-9am"]');
 var textArea10am = $('textarea[name="task-input-10am"]');
+var textArea11am = $('textarea[name="task-input-11am"]');
+var textArea12pm = $('textarea[name="task-input-12pm"]');
 
 // var buttonsEl = document.querySelector("button");
 var buttonsEl = document.getElementsByClassName("btn");
@@ -27,7 +29,7 @@ var currentHour = today.hour();
 var time9AM = moment("9", format).hour();
 var time10AM = moment("10", format).hour();
 var time11AM = moment("11", format).hour();
-var time12AM = moment("12", format).hour();
+var time12PM = moment("12", format).hour();
 var time1PM = moment("13", format).hour();
 var time2PM = moment("14", format).hour();
 var time3PM = moment("15", format).hour();
@@ -42,6 +44,7 @@ if (diff9AM > 0) {
 } else {
   textArea9am.addClass("present");
 }
+
 var diff10AM = currentHour - time10AM;
 if (diff10AM > 0) {
   textArea10am.addClass("past");
@@ -50,6 +53,25 @@ if (diff10AM > 0) {
 } else {
   textArea10am.addClass("present");
 }
+
+var diff11AM = currentHour - time11AM;
+if (diff11AM > 0) {
+  textArea11am.addClass("past");
+} else if (diff11AM < 0) {
+  textArea11am.addClass("future");
+} else {
+  textArea11am.addClass("present");
+}
+
+var diff12PM = currentHour - time12PM;
+if (diff12PM > 0) {
+  textArea12pm.addClass("past");
+} else if (diff12AM < 0) {
+  textArea12pm.addClass("future");
+} else {
+  textArea12pm.addClass("present");
+}
+
 ///////////////////////////////////////////////////////////////////////////////////////
 // Functions
 ///////////////////////////////////////////////////////////////////////////////////////
@@ -63,6 +85,8 @@ function retreiveTaskList() {
   taskList = storedTaskList;
   textArea9am.val(taskList[0]);
   textArea10am.val(taskList[1]);
+  textArea11am.val(taskList[2]);
+  textArea12pm.val(taskList[3]);
 }
 
 function init() {
@@ -82,6 +106,10 @@ for (var i = 0; i < buttonsEl.length; i++) {
       taskList[0] = textArea9am.val();
     } else if (buttonClicked.value == 1) {
       taskList[1] = textArea10am.val();
+    } else if (buttonClicked.value == 2) {
+      taskList[2] = textArea11am.val();
+    } else if (buttonClicked.value == 3) {
+      taskList[3] = textArea12pm.val();
     }
     storeTaskList();
   });
